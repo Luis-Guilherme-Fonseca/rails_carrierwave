51c69ebfbcd9d9b07c9e8194ed791a6bf713fe82
luisguilherme <luisfonseca@ejcm.com.br>
2017-09-26 08:04:15 -0300

    multiple files upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :body, presence: true
-  validates :image, file_size: { less_than: 1.megabytes } 
+  validates :image, file_size: { less_than: 1.megabytes }
+
   mount_uploader :image, ImageUploader
+  mount_uploader :attachments, ImageUploader
+  serialize :attachments, JSON
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925025423) do
+ActiveRecord::Schema.define(version: 20170926101910) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170925025423) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "attachments"
   end
 
 end
